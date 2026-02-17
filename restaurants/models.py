7724b3148cb9115dbb4d0a9cb8fffb456b3ee13f
Sandeep G L <sandeeplokesh44@gmail.com>
2026-02-18 00:29:50 +0530

    Initial working Food Delivery Application

--- restaurants/models.py
+++ restaurants/models.py
@@ -31,3 +31,9 @@ class MenuItem(models.Model):
 
     def __str__(self):
         return self.name
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
